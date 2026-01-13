f43fbddb7ef714acfc5b478775f3be2960f240d1
Ali1586 <keepitnice1990@gmail.com>
2026-01-13 22:18:58 +0100

    Meddelande

--- conftest.py
+++ conftest.py
@@ -1,26 +1,57 @@
+import time
+
 import playwright
 import pytest
 from playwright.sync_api import Playwright
 import pytest
 
 @pytest.fixture(scope="function")
-def set_up(browser) :
+def set_up(browser):
     #browser = playwright.chromium.launch(headless=False, slow_mo=500)
     context = browser.new_context()
     page = context.new_page()
     page.goto("https://symonstorozhenko.wixsite.com/website-1")
-    page.set_default_timeout(3000)
+    page.set_default_timeout(5000)
+
+    yield page
+    page.close()
+
+
+@pytest.fixture(scope="function")
+def login_set_up(set_up):
+
+    page=set_up
+    page.goto("")
+    page.set_default_timeout(5000)
+    login_issue = True
+    while login_issue:
+        if not page.is_visible("[data-testid=\"signUp.switchToSignUp\"]"):
+            page.click("button:has-text(\"Log In\")")
+        else:
+            login_issue = False
+        time.sleep(0.1)
+    page.click("[data-testid=\"signUp.switchToSignUp\"]", timeout=2000)
+    # page.click(":nth-match(:text('Log In'), 2)", timeout=2000)
+    page.click("[data-testid='switchToEmailLink'] >> [data-testid='buttonElement']")
+    # page.click("[data-testid='siteMembers.container'] input[type='email']")
+    # page.fill("[data-testid='siteMembers.container'] input[type='email']", "symon.storozhenko@gmail.com")
+    page.fill('input:below(:text("Email"))', "Keepitnice1990@gmail.com")
+    page.press("[data-testid='siteMembers.container'] >> input[type='email']", "Tab")
+    page.fill("input[type='password']", 'Asdfg12345!')
 
     yield page
 
 
 @pytest.fixture(scope="function")
-def login_set_up(set_up) :
-   # browser = playwright.chromium.launch(headless=False, slow_mo=500)
+def go_to_new_collections_page(set_up) :
+    #browser = playwright.chromium.launch(headless=False, slow_mo=500)
     #context = browser.new_context()
     #page = context.new_page()
-    #page.goto("https://symonstorozhenko.wixsite.com/website-1")
-    #page.set_default_timeout(3000)
-
+    page= set_up
+    page.goto("/new-collection")
+    page.set_default_timeout(5000)
 
     yield page
+
+
+
--- tests_ui_layout/test_new_login.py
+++ tests_ui_layout/test_new_login.py
@@ -1,37 +1,61 @@
 import re
-from playwright.sync_api import Playwright, sync_playwright, expect
+import time
+
 import pytest
+from playwright.sync_api import Playwright, sync_playwright, expect
 
 @pytest.mark.smoke
 @pytest.mark.regression
-def test_logged_user_can_view_my_orders_menu(login_set_up):
-   # browser = playwright.chromium.launch(headless=False, slow_mo=2000)
-    #context = browser.new_context()
-    #page.goto("https://symonstorozhenko.wixsite.com/website-1")
-    #page.set_default_timeout(12000)
-    #page.get_by_test_id("handle-button").click()
-    #page.get_by_test_id("signUp.switchToSignUp").click()
-    #page.get_by_role("button", name="Log in with Email").click()
-    #page.get_by_test_id("emailAuth").get_by_role("textbox", name="Email").fill("https://symonstorozhenko.wixsite.com/website-1%20@")
-    #page.get_by_test_id("emailAuth").get_by_role("textbox", name="Email").press("ArrowLeft")
-    #page.get_by_test_id("emailAuth").get_by_role("textbox", name="Email").fill("keepitnice1990@")
-    #page.get_by_test_id("emailAuth").get_by_role("textbox", name="Email").press("ArrowRight")
-    #page.get_by_test_id("emailAuth").get_by_role("textbox", name="Email").fill("keepitnice1990@gamil.com")
-    #page.get_by_role("textbox", name="Password").click()
-    #page.get_by_role("textbox", name="Password").fill("Asdfg12345!")
-    #page.get_by_test_id("submit").get_by_test_id("buttonElement").click()
-    #page.get_by_role("textbox", name="Password").dblclick()
-    #page.get_by_role("textbox", name="Password").fill("Asdfg12345!")
-    #page.get_by_test_id("emailAuth").get_by_role("textbox", name="Email").click()
-    #page.get_by_test_id("emailAuth").get_by_role("textbox", name="Email").fill("keepitnice1990@gmail.com")
-    #page.get_by_test_id("submit").get_by_test_id("buttonElement").click()
-    page=login_set_up
-    assert page.is_visiable("text=Contact Us")
+def test_login(login_set_up) -> None:
+        #browser = playwright.chromium.launch(headless=False, slow_mo=1000)
+        #context = browser.new_context()
+        page= login_set_up
+        #page = context.new_page()
+        #page.goto("https://symonstorozhenko.wixsite.com/website-1")
+        #page.set_default_timeout(14000)
+        login_issue = True
+        while login_issue:
+            if not page.is_visible("[data-testid=\"signUp.switchToSignUp\"]"):
+                page.click("button:has-text(\"Log In\")")
+            else:
+                login_issue = False
+            time.sleep(0.1)
+      #  page.get_by_test_id("handle-button").click()
+       # page.get_by_test_id("signUp.switchToSignUp").click(timeout=2000)
+       # page.wait_for_selector('[data-testid="signUp.switchToSignUp"]', timeout=10000)
 
-    # ---------------------
-    #context.close()
-    #browser.close()
+        #page.get_by_test_id("emailAuth").get_by_role("textbox", name="Email").fill("Keepitnice1990@")
+        #page.get_by_test_id("emailAuth").get_by_role("textbox", name="Email").press("ArrowRight")
+        #page.get_by_test_id("emailAuth").get_by_role("textbox", name="Email").fill("Keepitnice1990@gmail")
+        #page.get_by_role("textbox", name="Password").click()
+        #page.get_by_role("textbox", name="Password").fill("Asdfg12345")
+        #page.get_by_role("textbox", name="Password").press("CapsLock")
+        #page.get_by_role("textbox", name="Password").fill("Asdfg123451")
+        #page.get_by_test_id("emailAuth").get_by_role("textbox", name="Email").click()
+        #page.get_by_test_id("emailAuth").get_by_role("textbox", name="Email").press("ArrowRight")
+        #page.get_by_test_id("emailAuth").get_by_role("textbox", name="Email").press("ArrowRight")
+        #page.get_by_test_id("emailAuth").get_by_role("textbox", name="Email").fill("Keepitnice1990@gmail.com")
+        #page.get_by_test_id("submit").get_by_test_id("buttonElement").click()
+        #page.get_by_role("textbox", name="Password").click()
+        #page.get_by_role("textbox", name="Password").dblclick()
+        #page.get_by_role("textbox", name="Password").fill("Asdfg12345!")
+        #page.get_by_test_id("submit").get_by_test_id("buttonElement").click()
+        page.click("[data-testid=\"signUp.switchToSignUp\"]", timeout=2000)
+        # page.click(":nth-match(:text('Log In'), 2)", timeout=2000)
+        page.click("[data-testid='switchToEmailLink'] >> [data-testid='buttonElement']")
+        # page.click("[data-testid='siteMembers.container'] input[type='email']")
+        # page.fill("[data-testid='siteMembers.container'] input[type='email']", "symon.storozhenko@gmail.com")
+        page.fill('input:below(:text("Email"))', "Keepitnice1990@gmail.com")
+        page.press("[data-testid='siteMembers.container'] >> input[type='email']", "Tab")
+        page.fill("input[type='password']", 'Asdfg12345!')
+        page.click("[data-testid='submit'] >> [data-testid='buttonElement']")
+
+        assert page.is_visible("text= Home")
+
+        # ---------------------
+        #context.close()
+        #browser.close()
 
 
-#with sync_playwright() as playwright:
- #   run(playwright)
+    #with sync_playwright() as playwright:
+     #   run(playwright)
